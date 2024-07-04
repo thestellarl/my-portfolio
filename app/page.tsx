153a9603e90f7547de7cc89d1b8ed976f26f459d
Lucas <lucas.stella13@gmail.com>
2024-07-03 20:09:33 -0700

    added fadeIn

--- app/page.tsx
+++ app/page.tsx
@@ -78,19 +78,6 @@ function App(): React.ReactNode {
                   <a href="/">Lucas Stella</a>
                 </h1>
               </div>
-              <div className="h-full">
-                <ul className="h-[300%] flex flex-col header-scroll relative">
-                  <li className="basis-full flex items-center">
-                    Software Quality Assurance
-                  </li>
-                  <li className="basis-full flex items-center">
-                    Software Quality Assurance
-                  </li>
-                  <li className="basis-full flex items-center">
-                    Software Quality Assurance
-                  </li>
-                </ul>
-              </div>
             </header>
             <div className="pt-24 lg:w-1/2 lg:py-24 text-color3 px-5 experience-container">
               <section>
